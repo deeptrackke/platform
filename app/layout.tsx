3509aa7d6f6e4f713841cc6905600b4aa1d37cc5
Paul Ochieng <paulokello273@gmail.com>
2025-02-20 10:05:34 +0300

    Local fonts and hide eslint build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
-import { Outfit } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const outfit = Outfit({
-  variable: "--font-outfit",
-  subsets: ["latin"],
+const outfit = localFont({
+	src: "../fonts/Outfit.ttf",
+	variable: "--font-outfit",
+	display: "swap",
+	fallback: ["system-ui", "sans-serif"],
 });
 
 export const metadata: Metadata = {
